16b5887367341f0678f421dced849b3af1dd7f4f
paro82asparuh <paro82@gmail.com>
2017-09-25 17:44:14 +0300

    Commit_2017_09_25_1

---
+++ app/src/main/java/com/grigorov/asparuh/probujdane/BesediUpdateDialogFragment.java
@@ -0,0 +1,57 @@
+package com.grigorov.asparuh.probujdane;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by agr on 04/09/2017.
+ */
+
+public class BesediUpdateDialogFragment extends DialogFragment {
+
+    public interface BesediUpdateDialogListener {
+        void onBesediUpdateDialogPositiveClick(DialogFragment dialog);
+        void onBesediUpdateDialogNegativeClick(DialogFragment dialog);
+    }
+
+    BesediUpdateDialogListener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity = context instanceof Activity ? (Activity) context : null;
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (BesediUpdateDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement BesediUpdateDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_besedi_update)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onBesediUpdateDialogPositiveClick(BesediUpdateDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onBesediUpdateDialogNegativeClick(BesediUpdateDialogFragment.this);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
